76e757b8e62944d3d3e9f5350c9458812519b172
Liam Hales <liam.hales96@gmail.com>
2025-08-07 17:49:02 +0100

    Added `AppLayout` component used as the entry point to the app which renders the main `App` component and configures things such as fonts and app state

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import './globals.css';
+
+import { FunctionComponent, ReactElement, ReactNode } from 'react';
+import { BaseProps } from '../types';
+import { urbanist, firaCode } from '../fonts';
+import { Analytics } from '@vercel/analytics/next';
+import { App } from '../components';
+import { AppProvider } from '../providers';
+
+/**
+ * The `AppLayout` component props
+ */
+interface Props extends BaseProps {
+  readonly children: ReactNode;
+}
+
+/**
+ * The root layout component used as the entry point to the app which renders
+ * the main `App` component and configures things such as fonts and app state
+ *
+ * @param props The component props
+ * @returns The `AppLayout` component
+ */
+const AppLayout: FunctionComponent<Props> = ({ children }): ReactElement<Props> => {
+  return (
+    <html
+      lang="en"
+      className={`h-full ${urbanist.variable} ${firaCode.variable}`}
+    >
+      <body className="h-full bg-black">
+        <Analytics />
+        <AppProvider>
+          <App>
+            {children}
+          </App>
+        </AppProvider>
+      </body>
+    </html>
+  );
+};
+
+export default AppLayout;
