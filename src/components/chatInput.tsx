4d8d7274fae26cb5d5d67d3eb0ca624ba879d8dc
Liam Hales <liam.hales96@gmail.com>
2025-08-07 23:25:46 +0100

    Added `ChatInput` component used to render the chat input which allows the user to enter text used to chat with an LLM

---
+++ src/components/chatInput.tsx
@@ -0,0 +1,80 @@
+import { FunctionComponent, KeyboardEvent, ReactElement } from 'react';
+import { AIModel, BaseProps } from '../types';
+import { aiModels } from '../constants';
+
+/**
+ * The `ChatInput` component props
+ */
+interface Props extends BaseProps<HTMLInputElement> {
+  readonly value: string;
+  readonly model: AIModel;
+  readonly isDisabled?: boolean;
+  readonly onChange: (value: string) => void;
+  readonly onModelChange: (value: AIModel) => void;
+  readonly onKeyDown: (event: KeyboardEvent<HTMLInputElement>) => void;
+}
+
+/**
+ * Used to render the chat input which allows the user
+ * to enter text used to chat with an LLM
+ *
+ * @param props The component props
+ * @returns The `ChatInput` component
+ */
+const ChatInput: FunctionComponent<Props> = (props): ReactElement<Props> => {
+  const {
+    value,
+    model,
+    isDisabled = false,
+    onChange,
+    onModelChange,
+    onKeyDown,
+  } = props;
+
+  return (
+    <div className="w-full bg-zinc-950 border-solid border-[1px] rounded-lg border-zinc-900">
+      <div className="flex flex-col pt-5 pb-6 pl-6 pr-6">
+        <input
+          className="h-6 text-white placeholder-zinc-600 font-sans text-md bg-transparent outline-none"
+          placeholder="Chat with AI, ask anything you like"
+          value={value}
+          disabled={isDisabled}
+          onKeyDown={(event) => onKeyDown(event)}
+          onChange={(event) => {
+
+            // Destructure the event and the event target
+            // and pass its value to `onChange`
+            const { target } = event;
+            const { value } = target;
+
+            onChange(value);
+          }}
+        />
+        <div className="flex flex-col pt-8">
+          <div className="flex flex-row items-center gap-x-3">
+            {
+              aiModels.map((name) => {
+                return (
+                  <div
+                    className={`cursor-pointer bg-zinc-${(model === name) ? 700 : 900} border-solid border-[1px] rounded-lg border-zinc-${(model === name) ? 500 : 800} pt-2 pb-2 pl-3 pr-3`}
+                    key={name}
+                    role="button"
+                    tabIndex={0}
+                    onClick={() => onModelChange(name)}
+                    onKeyDown={() => onModelChange(name)}
+                  >
+                    <p className="font-mono text-xs text-white">
+                      {name}
+                    </p>
+                  </div>
+                );
+              })
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatInput;
