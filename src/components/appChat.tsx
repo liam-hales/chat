1916829e06dd0bb80775fd014464925a130947e5
Liam Hales <liam.hales96@gmail.com>
2025-08-09 18:35:11 +0100

    Added styling for chat messages

--- src/components/appChat.tsx
+++ src/components/appChat.tsx
@@ -4,6 +4,7 @@ import { FunctionComponent, ReactElement, useEffect, useMemo, useRef } from 'rea
 import { ChatInput } from '.';
 import { useApp } from '../hooks';
 import { BaseProps } from '../types';
+import { Loader } from './common';
 
 /**
  * The `AppChat` component props
@@ -34,8 +35,32 @@ const AppChat: FunctionComponent<Props> = ({ id }): ReactElement<Props> => {
   }, [id]);
 
   return (
-    <div className="w-full h-full max-w-[910px] flex flex-col items-center justify-between pb-10">
-      <div />
+    <div className="w-full h-[calc(100%-56px)] max-w-[910px] flex flex-col items-center justify-between pb-4 pl-4 pr-4">
+      <div className="w-full flex flex-col items-center gap-y-5 overflow-y-auto pt-10 pb-10 pl-3 pr-3">
+        {
+          messages.map((message, index) => {
+            const { role, content } = message;
+
+            return (role === 'user')
+              ? (
+                  <div
+                    className="self-end bg-zinc-900 border-solid border-[1px] border-zinc-800 rounded-2xl pt-2 pb-2 pl-4 pr-4"
+                    key={index}
+                  >
+                    <p className="text-white">{content}</p>
+                  </div>
+                )
+              : (
+                  <div
+                    className="self-start"
+                    key={index}
+                  >
+                    <p className="text-white">{content}</p>
+                  </div>
+                );
+          })
+        }
+      </div>
       <ChatInput
         ref={inputRef}
         value={inputValue}
