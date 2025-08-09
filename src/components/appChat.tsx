4e2ba5487583b4a80f1919393222d186a457c8ca
Liam Hales <liam.hales96@gmail.com>
2025-08-09 01:28:59 +0100

    Renamed `chatId` prop to `id`

--- src/components/appChat.tsx
+++ src/components/appChat.tsx
@@ -9,7 +9,7 @@ import { BaseProps } from '../types';
  * The `AppChat` component props
  */
 interface Props extends BaseProps {
-  readonly chatId: string;
+  readonly id: string;
 }
 
 /**
@@ -19,10 +19,10 @@ interface Props extends BaseProps {
  * @param props The component props
  * @returns The `AppChat` component
  */
-const AppChat: FunctionComponent<Props> = ({ chatId }): ReactElement<Props> => {
+const AppChat: FunctionComponent<Props> = ({ id }): ReactElement<Props> => {
 
   const { getChat, setInputValue, setModel, sendMessage } = useApp();
-  const { model, inputValue, messages } = useMemo(() => getChat(chatId), [chatId, getChat]);
+  const { model, inputValue, messages } = useMemo(() => getChat(id), [id, getChat]);
 
   return (
     <div className="w-full h-full max-w-[910px] flex flex-col items-center justify-between pb-10">
@@ -31,9 +31,9 @@ const AppChat: FunctionComponent<Props> = ({ chatId }): ReactElement<Props> => {
         value={inputValue}
         model={model}
         allowModelSelect={messages.length === 0}
-        onChange={(value) => setInputValue(chatId, value)}
-        onModelChange={(value) => setModel(chatId, value)}
-        onSend={() => sendMessage(chatId)}
+        onChange={(value) => setInputValue(id, value)}
+        onModelChange={(value) => setModel(id, value)}
+        onSend={() => sendMessage(id)}
       />
     </div>
   );
