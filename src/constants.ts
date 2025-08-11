c32793f6f38638fe948af71c6118c73c86294f5a
Liam Hales <liam.hales96@gmail.com>
2025-08-11 12:24:52 +0100

    Added `limits` prop to model definitions to specify restrictions for each model

--- src/constants.ts
+++ src/constants.ts
@@ -9,30 +9,44 @@ export const aiModelDefinitions = [
     id: nanoid(8),
     name: 'gpt-oss-20b',
     openRouterId: 'openai/gpt-oss-20b:free',
+    limits: null,
   },
   {
     id: nanoid(8),
     name: 'gpt-oss-120b',
     openRouterId: 'openai/gpt-oss-120b',
+    limits: {
+      outputTokens: 1024,
+      messageLength: 4096,
+      chatLength: 10,
+    },
   },
   {
     id: nanoid(8),
     name: 'gpt-5-mini',
     openRouterId: 'openai/gpt-5-mini',
+    limits: {
+      outputTokens: 1024,
+      messageLength: 4096,
+      chatLength: 10,
+    },
   },
   {
     id: nanoid(8),
     name: 'deepseek-r1',
     openRouterId: 'deepseek/deepseek-r1-0528:free',
+    limits: null,
   },
   {
     id: nanoid(8),
     name: 'gemma-3-27b',
     openRouterId: 'google/gemma-3-27b-it:free',
+    limits: null,
   },
   {
     id: nanoid(8),
     name: 'gemma-3n-2b',
     openRouterId: 'google/gemma-3n-e2b-it:free',
+    limits: null,
   },
 ] as const;
