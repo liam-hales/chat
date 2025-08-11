4eb719c533d859d34bc54a9cbd88319e345aa282
Liam Hales <liam.hales96@gmail.com>
2025-08-11 12:25:47 +0100

    Updated `streamChatSchema` to build a schema for each model taking into account its `limits`

--- src/schemas/streamChat.schema.ts
+++ src/schemas/streamChat.schema.ts
@@ -1,28 +1,72 @@
 import { z } from 'zod';
 import { aiModelDefinitions } from '../constants';
+import { AIModelDefinition } from '../types';
+
+/**
+ * Used to build the schema for a specific model
+ * `definition` taking into account its `limits`
+ *
+ * @param definition The model definition
+ * @returns The schema
+ */
+const _buildSchema = (definition: AIModelDefinition) => {
+  const { openRouterId, limits } = definition;
+
+  const content = z
+    .string()
+    .min(1);
+
+  // If the model has limits then set a
+  // maximum character length for the content
+  if (limits != null) {
+    content.max(limits.messageLength);
+  }
+
+  const messages = z
+    .array(
+      z
+        .object({
+          role: z.union([
+            z.literal('user'),
+            z.literal('assistant'),
+          ]),
+          content: content,
+        })
+        .strict(),
+    )
+    .min(1);
+
+  // If the model has limits then set a
+  // maximum length for the messages array
+  if (limits != null) {
+    messages.max(limits.chatLength);
+  }
+
+  return z
+    .object({
+      modelId: z.literal(openRouterId),
+      messages: messages,
+      maxOutputTokens: (limits != null)
+        ? z
+            .number()
+            .min(1)
+            .max(limits.outputTokens)
+            .default(limits.outputTokens)
+            .optional()
+        : z
+            .number()
+            .min(1)
+            .optional(),
+    })
+    .strict();
+};
 
 /**
  * The schema for the `streamChat` helper used
  * for type inference and to validate options
  */
-const streamChatSchema = z.object({
-  modelId: z.union(
-    aiModelDefinitions.map((model) => z.literal(model.openRouterId)),
-  ),
-  messages: z
-    .array(
-      z.object({
-        role: z.union([
-          z.literal('user'),
-          z.literal('assistant'),
-        ]),
-        content: z.string()
-          .min(1)
-          .max(4096),
-      }),
-    )
-    .min(1)
-    .max(10),
-});
+const streamChatSchema = z.union(
+  aiModelDefinitions.map((definition) => _buildSchema(definition)),
+);
 
 export default streamChatSchema;
