1779be37c4420517846d60fb2305d07e43cf460c
Liam Hales <liam.hales96@gmail.com>
2025-08-20 23:39:11 +0100

    Added `isDefault` prop to AI model definitions

--- src/constants.ts
+++ src/constants.ts
@@ -9,12 +9,14 @@ export const aiModelDefinitions = [
     id: nanoid(8),
     name: 'gpt-oss-20b',
     openRouterId: 'openai/gpt-oss-20b:free',
+    isDefault: true,
     limits: null,
   },
   {
     id: nanoid(8),
     name: 'gpt-oss-120b',
     openRouterId: 'openai/gpt-oss-120b',
+    isDefault: false,
     limits: {
       outputTokens: 1024,
       messageLength: 4096,
@@ -25,6 +27,7 @@ export const aiModelDefinitions = [
     id: nanoid(8),
     name: 'gpt-5-mini',
     openRouterId: 'openai/gpt-5-mini',
+    isDefault: false,
     limits: {
       outputTokens: 1024,
       messageLength: 4096,
@@ -35,18 +38,21 @@ export const aiModelDefinitions = [
     id: nanoid(8),
     name: 'deepseek-r1',
     openRouterId: 'deepseek/deepseek-r1-0528:free',
+    isDefault: false,
     limits: null,
   },
   {
     id: nanoid(8),
     name: 'gemma-3-27b',
     openRouterId: 'google/gemma-3-27b-it:free',
+    isDefault: false,
     limits: null,
   },
   {
     id: nanoid(8),
     name: 'gemma-3n-2b',
     openRouterId: 'google/gemma-3n-e2b-it:free',
+    isDefault: false,
     limits: null,
   },
 ] as const;
