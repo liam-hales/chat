cd84e2e1d349e37c3235503cbd9b9f6a65cde4c6
Liam Hales <liam.hales96@gmail.com>
2025-08-20 23:35:45 +0100

    Added `size` and `showDefaultLabel` props to `Model` component and updated styling

--- src/components/model.tsx
+++ src/components/model.tsx
@@ -8,6 +8,8 @@ import { BadgePoundSterling } from 'lucide-react';
 interface Props extends BaseProps {
   readonly definition: AIModelDefinition;
   readonly appearance?: 'light' | 'dark';
+  readonly size?: 'small' | 'large';
+  readonly showDefaultLabel?: boolean;
   readonly onClick?: () => void;
 }
 
@@ -18,35 +20,58 @@ interface Props extends BaseProps {
  * @param props The component props
  * @retruns The `Model` component
  */
-const Model: FunctionComponent<Props> = ({ definition, appearance = 'dark', onClick }): ReactElement<Props> => {
-  const { name, limits } = definition;
+const Model: FunctionComponent<Props> = (props): ReactElement<Props> => {
+  const {
+    className,
+    definition,
+    appearance = 'dark',
+    size = 'small',
+    showDefaultLabel = false,
+    onClick,
+  } = props;
+  const { name, limits, isDefault } = definition;
 
   return (
-    <button
+    <div
       className={`
-        border-solid border-[1px] rounded-md pt-1 pb-1 pl-2 pr-2
+        ${className ?? ''}
+
+        border-solid border-[1px] rounded-md
+
+        ${(size === 'small') ? 'pt-1 pb-1 pl-2 pr-2' : 'pt-2 pb-2 pl-3 pr-3'}
 
         ${(appearance === 'light') ? 'bg-zinc-500' : 'bg-zinc-900'}
         ${(appearance === 'light') ? 'border-zinc-400' : 'border-zinc-800'}
 
         ${(onClick != null) && 'cursor-pointer'}
       `}
+      role={(onClick != null) ? 'button' : undefined}
+      tabIndex={0}
       onClick={onClick}
+      onKeyDown={onClick}
     >
       <div className="flex flex-row items-center gap-x-2">
-        <p className="font-mono text-white text-[11px]">
-          {name}
+        <p className={`
+          font-mono text-white
+          ${(size === 'small') ? 'text-[11px]' : 'text-xs'}
+        `}
+        >
+          {
+            (isDefault === true && showDefaultLabel === true)
+              ? `${name} (Default)`
+              : name
+          }
         </p>
         {
           (limits != null) && (
             <BadgePoundSterling
               className="text-white"
-              size={14}
+              size={(size === 'small') ? 14 : 16}
             />
           )
         }
       </div>
-    </button>
+    </div>
   );
 };
 
